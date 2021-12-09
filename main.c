b0a60a2d4c1c54826af51442345ecaf7c782dbc9
Marcus Nilsson <brainbomb@gmail.com>
2021-12-09 10:56:06 +0100

    Test new getter

--- main.c
+++ main.c
@@ -15,14 +15,11 @@ int main(int argc, char **argv) {
   uint16_t temp;
   mcp9600_read_hot(&handle, &temp);
 
+  float f_data;
+  mcp9600_read_temp(&handle, HOT, &f_data);
+
   printf("Temp: %x\n", temp);
-
-  int upper = temp & 0xff;
-  int lower = (temp >> 8) & 0xff;
-
-  float f_temp = upper * 16.0f + (lower / 16.0f);
-
-  printf("Temp: %f\n", f_temp);
+  printf("Temp: %f\n", f_data);
 
   err = mcp9600_deinit(&handle);
 
