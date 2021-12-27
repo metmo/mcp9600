8223c314244c6c9abd51fcc3bb40d68fe207c7f2
Marcus Nilsson <brainbomb@gmail.com>
2021-12-27 09:17:29 +0100

    Put register defines in an enum

--- src/mcp9600-driver.h
+++ src/mcp9600-driver.h
@@ -7,32 +7,6 @@
 extern "C" {
 #endif
 
-#define MCP9600_REG_TH 0x00
-#define MCP9600_REG_TD 0x01
-#define MCP9600_REG_TC 0x02
-
-#define MCP9600_REG_RAW 0x03
-#define MCP9600_REG_STATUS 0x04
-#define MCP9600_REG_TCONF 0x05
-#define MCP9600_REG_DCONF 0x06
-
-#define MCP9600_REG_ALERT1 0x08
-#define MCP9600_REG_ALERT2 0x09
-#define MCP9600_REG_ALERT3 0x0a
-#define MCP9600_REG_ALERT4 0x0b
-
-#define MCP9600_REG_T_HYST1 0x0c
-#define MCP9600_REG_T_HYST2 0x0d
-#define MCP9600_REG_T_HYST3 0x0e
-#define MCP9600_REG_T_HYST4 0x0f
-
-#define MCP9600_REG_T_ALERT1 0x10
-#define MCP9600_REG_T_ALERT2 0x11
-#define MCP9600_REG_T_ALERT3 0x12
-#define MCP9600_REG_T_ALERT4 0x13
-
-#define MCP9600_REG_DEV_ID 0x20
-
 #define MCP9600_STATUS_REG_BIT_BURSTCLPT (1 << 7)
 #define MCP9600_STATUS_REG_BIT_THUPDATE (1 << 6)
 #define MCP9600_STATUS_REG_BIT_SC (1 << 5)
@@ -59,47 +33,74 @@ extern "C" {
 #define MCP9600_ALERT_REG_BIT_COMPMODE (1 << 1)
 #define MCP9600_ALERT_REG_BIT_ENABLE (1 << 0)
 
+typedef enum mcp9600_registers {
+    MCP9600_REG_TH = 0x00,
+    MCP9600_REG_TD = 0x01,
+    MCP9600_REG_TC = 0x02,
+
+    MCP9600_REG_RAW = 0x03,
+    MCP9600_REG_STATUS = 0x04,
+    MCP9600_REG_TCONF = 0x05,
+    MCP9600_REG_DCONF = 0x06,
+
+    MCP9600_REG_ALERT1 = 0x08,
+    MCP9600_REG_ALERT2 = 0x09,
+    MCP9600_REG_ALERT3 = 0x0a,
+    MCP9600_REG_ALERT4 = 0x0b,
+
+    MCP9600_REG_T_HYST1 = 0x0c,
+    MCP9600_REG_T_HYST2 = 0x0d,
+    MCP9600_REG_T_HYST3 = 0x0e,
+    MCP9600_REG_T_HYST4 = 0x0f,
+
+    MCP9600_REG_T_ALERT1 = 0x10,
+    MCP9600_REG_T_ALERT2 = 0x11,
+    MCP9600_REG_T_ALERT3 = 0x12,
+    MCP9600_REG_T_ALERT4 = 0x13,
+    MCP9600_REG_DEV_ID = 0x20
+} mcp9600_registers_t;
+
 typedef enum mcp9600_thermocouple {
-  TYPE_K = 0x00,
-  TYPE_J = 0x01,
-  TYPE_T = 0x02,
-  TYPE_N = 0x03,
-  TYPE_S = 0x04,
-  TYPE_E = 0x05,
-  TYPE_B = 0x06,
-  TYPE_R = 0x07,
+    TYPE_K = 0x00,
+    TYPE_J = 0x01,
+    TYPE_T = 0x02,
+    TYPE_N = 0x03,
+    TYPE_S = 0x04,
+    TYPE_E = 0x05,
+    TYPE_B = 0x06,
+    TYPE_R = 0x07,
 } mcp9600_thermocouple_t;
 
 typedef enum mcp9600_resolution {
-  RES_18 = 0x00,
-  RES_16 = 0x01,
-  RES_14 = 0x02,
-  RES_12 = 0x03,
+    RES_18 = 0x00,
+    RES_16 = 0x01,
+    RES_14 = 0x02,
+    RES_12 = 0x03,
 } mcp9600_resolution_t;
 
 typedef enum mcp9600_thermocouple_reg {
-  HOT = MCP9600_REG_TH,
-  DELTA = MCP9600_REG_TD,
-  COLD = MCP9600_REG_TC,
+    HOT = MCP9600_REG_TH,
+    DELTA = MCP9600_REG_TD,
+    COLD = MCP9600_REG_TC,
 } mcp9600_thermocouple_reg_t;
 
 typedef enum mcp9600_filter_coefficients {
-  FILTER_OFF = 0x00,
-  FILTER_1 = 0x01,
-  FILTER_2 = 0x02,
-  FILTER_3 = 0x03,
-  FILTER_4 = 0x04,
-  FILTER_5 = 0x05,
-  FILTER_6 = 0x06,
-  FILTER_7 = 0x07,
+    FILTER_OFF = 0x00,
+    FILTER_1 = 0x01,
+    FILTER_2 = 0x02,
+    FILTER_3 = 0x03,
+    FILTER_4 = 0x04,
+    FILTER_5 = 0x05,
+    FILTER_6 = 0x06,
+    FILTER_7 = 0x07,
 } mcp9600_filter_coefficients_t;
 
 typedef struct mcp9600_handle {
-  int fd;
-  uint8_t i2c_addr;
-  mcp9600_thermocouple_t tc_type;
-  mcp9600_resolution_t resolution;
-  mcp9600_filter_coefficients_t filter;
+    int fd;
+    uint8_t i2c_addr;
+    mcp9600_thermocouple_t tc_type;
+    mcp9600_resolution_t resolution;
+    mcp9600_filter_coefficients_t filter;
 } mcp9600_handle_t;
 
 uint8_t mcp9600_init(mcp9600_handle_t *handle, char *device, uint8_t i2c_addr,
